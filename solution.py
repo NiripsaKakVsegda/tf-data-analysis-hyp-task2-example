d24e9998ec0a3c58164ba5741065a3fa0311fc4c
NiripsaKakVsegda <anna.poluyanenko@gmail.com>
2023-04-11 01:02:48 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import cramervonmises_2samp
 
 chat_id = 440047378
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_value = ks_2samp(x, y).pvalue
+    p_value = cramervonmises_2samp(x, y).pvalue
     return p_value < 0.01
