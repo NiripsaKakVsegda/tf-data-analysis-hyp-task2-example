0f18a3941248087fcac0317efa6fc99c2c09658e
NiripsaKakVsegda <anna.poluyanenko@gmail.com>
2023-04-12 02:58:34 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import anderson_ksamp
+from hyppo.ksample import MMD
 
 chat_id = 440047378
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_value = anderson_ksamp([x, y]).pvalue
+    p_value = MMD(compute_kernel="laplacian", gamma=10).test(x, y)[1]
     return p_value < 0.01
